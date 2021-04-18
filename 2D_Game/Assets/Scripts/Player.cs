2714998bc1fdc26dc7a0962632c1b00b225a6c4a
Lulawaai <57148554+Lulawaai@users.noreply.github.com>
2021-04-18 23:41:06 +0200

    Player movement

---
+++ 2D_Game/Assets/Scripts/Player.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+	[SerializeField] private float _speedPlayer;
+
+	// Start is called before the first frame update
+	void Start()
+	{
+
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		Movement();
+    }
+
+	private void Movement()
+	{
+		float moveX = Input.GetAxis("Horizontal");
+		float moveY = Input.GetAxis("Vertical");
+
+		Vector3 direction = new Vector3(moveX, moveY, 0);
+
+		transform.Translate(direction * _speedPlayer * Time.deltaTime);
+
+		transform.position = new Vector3(Mathf.Clamp(transform.position.x, -9f, 0f), transform.position.y, 0);
+	
+
+		if (transform.position.y > 6.8f)
+		{
+			transform.position = new Vector3(transform.position.x, -5f, 0);
+		}
+
+		else if (transform.position.y < -5)
+		{
+			transform.position = new Vector3(transform.position.x, 6.8f, 0);
+		}
+	}
+}
